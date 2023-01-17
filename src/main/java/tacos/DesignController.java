7b4931aedf2f8f4aeb5604d61e05ea4cea3b9269
dongeranguk <rooetym@naver.com>
2023-01-17 22:14:11 +0900

    #1-Controller 및 View 생성 식자재(Ingredient) 도메인 클래스를 model 속성으로 뷰에 전달하여, 뷰에 잘 나타나는지 확인하였고, 식자재들을 고른 뒤 주문 정보를 입력하여 최종적으로 Taco 객체를 만들어냈다. 이들의 정보를 입력할 때에는 하이버네이트의 어노테이션을 선언하여, 입력한 정보의 유효성 검사를 하였고, 에러 메시지가 속성과 매핑되어 잘 나타나는지 확인하였다.

--- src/main/java/tacos/DesignController.java
+++ src/main/java/tacos/DesignController.java
@@ -2,6 +2,7 @@ package tacos;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import tacos.Taco;
-import tacos.Ingredient;
 import tacos.Ingredient.Type;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/design")
@@ -50,7 +51,10 @@ public class DesignController {
     }
 
     @PostMapping
-    public String order() {
-        return "redirect:order/currents";
+    public String preocessDesign(@Valid Taco design, Errors errors) {
+        if(errors.hasErrors()) {
+            return "design";
+        }
+        return "redirect:orders/currents";
     }
 }
