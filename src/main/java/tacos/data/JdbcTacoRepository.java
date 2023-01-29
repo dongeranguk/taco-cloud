fbe7a496edbd07d842ebda5dba5b682478afc2f8
dongeranguk <rooetym@naver.com>
2023-01-29 23:56:00 +0900

    #4-jpa jpa 사용하여 DB의 테이블과 매핑되도록 Entity 설정

--- src/main/java/tacos/data/JdbcTacoRepository.java
+++ src/main/java/tacos/data/JdbcTacoRepository.java
@@ -29,7 +29,7 @@ public class JdbcTacoRepository implements TacoRepository {
     public Taco save(Taco taco) {
         long tacoId = saveTacoInfo(taco);
         taco.setId(tacoId);
-        for(Ingredient ingredient : taco.getIngredients()) {
+            for(Ingredient ingredient : taco.getIngredients()) {
             saveIngredientToTaco(ingredient, tacoId);
         }
         return taco;
@@ -37,14 +37,12 @@ public class JdbcTacoRepository implements TacoRepository {
 
     private long saveTacoInfo(Taco taco) {
         taco.setCreatedAt(new Date());
-        PreparedStatementCreator psc =
-                new PreparedStatementCreatorFactory(
-                        "insert into Taco (name, createdAt) values (?, ?) ",
-                        Types.VARCHAR, Types.TIMESTAMP
-                ).newPreparedStatementCreator(
-                        Arrays.asList(
-                                taco.getName(),
-                                new Timestamp(taco.getCreatedAt().getTime())));
+        PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(
+                "insert into Taco (name, createdAt) values (?, ?) ",
+                        Types.VARCHAR, Types.TIMESTAMP);
+        pscf.setReturnGeneratedKeys(Boolean.TRUE);
+        PreparedStatementCreator psc = pscf.newPreparedStatementCreator(
+                Arrays.asList(taco.getName(), new Timestamp(taco.getCreatedAt().getTime())));
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
         jdbc.update(psc, keyHolder);
